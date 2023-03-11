162f27647da6926f0a352dad93163668dcf3b064
A-simpa <abdulmujeebm@yahoo.com>
2023-03-11 18:53:37 +0100

    fix: remove second malloc

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,10 +22,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	node->n = value;
 	node->parent = parent;
 	node->right = NULL;
-	if (parent->left)
-		node->left = parent->left;
-	else
+	if (!(parent->left))
 		node->left = NULL;
+	else
+	{
+		parent->left->parent = node;
+		node->left = parent->left;
+	}
 	parent->left = node;
 
 	return (node);
