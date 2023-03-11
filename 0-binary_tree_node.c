9c7eb5c8557775ea52c8d6d32ee8c3237ab582aa
A-simpa <abdulmujeebm@yahoo.com>
2023-03-11 16:55:29 +0100

    initialized children

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,6 +15,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	node = malloc(sizeof(binary_tree_t));
 	node->parent = parent;
 	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
 	return (node);
 }
 
