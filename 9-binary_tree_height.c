84562dd7fd363b661447c0072fc19cdcc07e0c66
A-simpa <abdulmujeebm@yahoo.com>
2023-03-12 16:14:19 +0100

    fix and refactor: added isleaf function and condition for going down the tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,20 @@
 #include "binary_trees.h"
+/**
+ * isleaf - checks if a tree is a leaf
+ *
+ * @node: pointer to the node to check
+ * Return: 1 for TRUE 0 for FALSE
+ */
+
+int isleaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	else if (!(node->left == NULL && node->right == NULL))
+		return (0);
+	return (1);
+}
+
 
 /**
  * binary_tree_incount - pass each node of a binary tree to func
@@ -17,15 +33,15 @@ int binary_tree_incount(const binary_tree_t *tree, int start, int level)
 
 	if (tree)
 	{
-		start = binary_tree_incount(tree->left, start, level);
-		start = binary_tree_incount(tree->right, start, level);
+		if (tree->left)
+			start = binary_tree_incount(tree->left, start, level);
+		if (tree->right)
+			start = binary_tree_incount(tree->right, start, level);
 	}
-	if (!tree)
-		return (0);
-	while (buf->parent)
+	if (isleaf(buf))
 	{
-		buf = buf->parent;
-		count++;
+		while (buf->parent)
+			buf = buf->parent, count++;
 	}
 	if (count - level > start)
 		start = count -  level;
